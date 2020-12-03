9071205648d77085d56f64bd72907887040f77ef
v-zhangjianqing <v-zhangjianqing@xiaomi.com>
2020-12-03 10:35:44 +0800

    一轮开发完成

--- app/src/main/java/com/permissionx/app/MainActivity.kt
+++ app/src/main/java/com/permissionx/app/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.permissionx.app
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.permissionx.qing.PermissionX
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        makeCallBtn.setOnClickListener {
+            PermissionX.request(this, Manifest.permission.CALL_PHONE) { allGranted, deniedList ->
+                if (allGranted) {
+                    call()
+                } else {
+                    Toast.makeText(this, "You denied $deniedList", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+
+//        //多个权限
+//        PermissionX.request(this, Manifest.permission.CALL_PHONE,Manifest.permission.READ_CONTACTS){
+//
+//        }
+
+    }
+
+    private fun call() {
+        val intent=Intent(Intent.ACTION_CALL)
+        intent.data=Uri.parse("tel:10086")
+        startActivity(intent)
     }
 }
